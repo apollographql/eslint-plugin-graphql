f609ed1a571b35ccf23f616d6e888981566ef484
Sashko Stubailo <sashko@stubailo.com>
2016-04-21 21:20:17 -0700

    Refactor

--- graphql-template-string.js
+++ graphql-template-string.js
@@ -1,84 +1,56 @@
 import {
   parse,
-  buildASTSchema,
   validate,
 } from 'graphql';
 
-const typeDefinition = `
-  schema {
-    query: RootQuery
+export function makeRule({
+  schema,
+  tagName = 'gql',
+}) {
+  return function (context) {
+    return {
+      TaggedTemplateExpression(node) {
+        if (node.tag.name !== tagName) {
+          return;
+        }
+
+        if (node.quasi.quasis.length > 1) {
+          context.report({
+            node,
+            message: 'Unexpected interpolation in GraphQL template string.',
+          });
+
+          return;
+        }
+
+        const text = node.quasi.quasis[0].value.cooked;
+
+        let ast;
+
+        try {
+          ast = parse(text);
+        } catch (error) {
+          context.report({
+            node,
+            message: error.message.split('\n')[0],
+            loc: locFrom(node, error),
+          });
+          return;
+        }
+
+        const validationErrors = schema ? validate(schema, ast) : [];
+
+        if (validationErrors && validationErrors.length > 0) {
+          context.report({
+            node,
+            message: validationErrors[0].message,
+            loc: locFrom(node, validationErrors[0]),
+          });
+        }
+      }
+    };
   }
-
-  type RootQuery {
-    number: Int
-  }
-`;
-
-const schema = buildASTSchema(parse(typeDefinition));
-
-//------------------------------------------------------------------------------
-// Rule Definition
-//------------------------------------------------------------------------------
-
-module.exports = function (context) {
-  // variables should be defined here
-
-  //--------------------------------------------------------------------------
-  // Helpers
-  //--------------------------------------------------------------------------
-
-  // any helper functions should go here or else delete this section
-
-  //--------------------------------------------------------------------------
-  // Public
-  //--------------------------------------------------------------------------
-
-  return {
-    TaggedTemplateExpression(node) {
-      if (node.tag.name !== 'gql') {
-        return;
-      }
-
-      if (node.quasi.quasis.length > 1) {
-        context.report({
-          node,
-          message: 'Unexpected interpolation in GraphQL template string.',
-        });
-
-        return;
-      }
-
-      const text = node.quasi.quasis[0].value.cooked;
-
-      let ast;
-
-      try {
-        ast = parse(text);
-      } catch (error) {
-        context.report({
-          node,
-          message: error.message.split('\n')[0],
-          loc: locFrom(node, error),
-        });
-        return;
-      }
-
-      const validationErrors = schema ? validate(schema, ast) : [];
-
-      if (validationErrors && validationErrors.length > 0) {
-        context.report({
-          node,
-          message: validationErrors[0].message,
-          loc: locFrom(node, validationErrors[0]),
-        });
-      }
-    }
-  };
-};
-
-module.exports.schema = [
-    // fill in your schema
-];
+}
 
 function locFrom(node, error) {
   const location = error.locations[0];
--- test/graphql-template-string.js
+++ test/graphql-template-string.js
@@ -1,5 +1,32 @@
-const rule = require('../graphql-template-string');
-const RuleTester = require('eslint').RuleTester;
+import {
+  parse,
+  buildASTSchema,
+} from 'graphql';
+
+import { makeRule } from '../graphql-template-string';
+import { RuleTester } from 'eslint';
+
+// Set up a fake schema for tests
+
+const typeDefinition = `
+  schema {
+    query: RootQuery
+  }
+
+  type RootQuery {
+    number: Int
+  }
+`;
+
+const schema = buildASTSchema(parse(typeDefinition));
+
+// Init rule
+
+const rule = makeRule({
+  schema,
+});
+
+// Set up tests
 
 const ruleTester = new RuleTester();
 
@@ -8,7 +35,7 @@ const parserOptions = {
   'sourceType': 'module',
 };
 
-ruleTester.run('graphql-template-string', rule, {
+ruleTester.run('default options', rule, {
   valid: [
     {
       parserOptions,
@@ -36,6 +63,47 @@ ruleTester.run('graphql-template-string', rule, {
     {
       parserOptions,
       code: 'const x = gql`{ nonExistentQuery }`',
+      errors: [{
+        message: 'Cannot query field "nonExistentQuery" on type "RootQuery".',
+        type: 'TaggedTemplateExpression'
+      }]
+    }
+  ]
+});
+
+const customRule = makeRule({
+  schema,
+  tagName: 'myGraphQLTag',
+});
+
+ruleTester.run('custom tag name', customRule, {
+  valid: [
+    {
+      parserOptions,
+      code: 'const x = myGraphQLTag`{ number }`',
+    },
+  ],
+
+  invalid: [
+    {
+      parserOptions,
+      code: 'const x = myGraphQLTag`{ ${x} }`',
+      errors: [{
+        message: 'Unexpected interpolation in GraphQL template string.',
+        type: 'TaggedTemplateExpression'
+      }]
+    },
+    {
+      parserOptions,
+      code: 'const x = myGraphQLTag``',
+      errors: [{
+        message: 'Syntax Error GraphQL (1:1) Unexpected EOF',
+        type: 'TaggedTemplateExpression'
+      }]
+    },
+    {
+      parserOptions,
+      code: 'const x = myGraphQLTag`{ nonExistentQuery }`',
       errors: [{
         message: 'Cannot query field "nonExistentQuery" on type "RootQuery".',
         type: 'TaggedTemplateExpression'
