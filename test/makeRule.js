342714f3f51e9be7bb0eb6f950867896114a8165
Sashko Stubailo <sashko@stubailo.com>
2016-04-27 11:46:06 -0700

    Add failing test for deprecation

--- test/makeRule.js
+++ test/makeRule.js
@@ -58,6 +58,15 @@ const parserOptions = {
           type: 'Identifier'
         }]
       },
+      {
+        options,
+        parserOptions,
+        code: 'const x = gql`{ deprecatedQuery }`',
+        errors: [{
+          message: 'Invalid interpolation - not a valid fragment or variable.',
+          type: 'Identifier'
+        }]
+      },
     ]
   });
 }
@@ -93,6 +102,15 @@ const parserOptions = {
         errors: [{
           message: 'Cannot query field "nonExistentQuery" on type "RootQuery".',
           type: 'TaggedTemplateExpression'
+        }]
+      },
+      {
+        options,
+        parserOptions,
+        code: 'const x = myGraphQLTag`{ ${x} }`',
+        errors: [{
+          type: 'Identifier',
+          message: 'Invalid interpolation - not a valid fragment or variable.'
         }]
       },
       {
--- test/updateSchemaJson.js
+++ test/updateSchemaJson.js
@@ -5,6 +5,18 @@ var graphql = require('graphql');
 var typeDefinition = fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8');
 var schema = graphql.buildASTSchema(graphql.parse(typeDefinition));
 
+var addResolveFunctionsToSchema = require('graphql-tools').addResolveFunctionsToSchema;
+
+var resolveFunctions = {
+  RootQuery: {
+    deprecatedQuery: {
+      __deprecationReason: "This field is deprecated. Why? Just because.",
+    },
+  },
+};
+
+addResolveFunctionsToSchema(schema, resolveFunctions);
+
 graphql.graphql(schema, graphql.introspectionQuery).then(function (result) {
   fs.writeFileSync(path.join(__dirname, 'schema.json'),
     JSON.stringify(result, null, 2));
