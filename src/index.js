ac6e7aad937f39524d9102e02e1511e4fcf1d06c
Julien Poissonnier <j.poissonnier@whatever-company.com>
2016-11-19 12:30:31 +0100

    Fix tagname check
    Don't try to validate template strings with a segemented tagname when
    the configured tagname is a single indentifier and vice versa.

--- src/index.js
+++ src/index.js
@@ -104,12 +104,12 @@ const rules = {
         const tagNameSegments = tagName.split('.').length;
         if (tagNameSegments === 1) {
           // Check for single identifier, like 'gql'
-          if (node.tag.type === 'Identifier' && node.tag.name !== tagName) {
+          if (node.tag.type !== 'Identifier' || node.tag.name !== tagName) {
             return;
           }
         } else if (tagNameSegments === 2){
           // Check for dotted identifier, like 'Relay.QL'
-          if (node.tag.type === 'MemberExpression' &&
+          if (node.tag.type !== 'MemberExpression' ||
               node.tag.object.name + '.' + node.tag.property.name !== tagName) {
             return;
           }
