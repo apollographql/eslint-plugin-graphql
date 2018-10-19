774f91bea42a79c573095051f9396957cb898c67
Pat Sissons <pat.sissons@shopify.com>
2018-10-19 12:54:38 -0700

    adding failing test

--- test/graphqlconfig/index.js
+++ test/graphqlconfig/index.js
@@ -87,4 +87,58 @@ describe('graphqlconfig support', () => {
       ],
     });
   });
+
+  describe('multiproject literal', () => {
+    beforeEach(() => {
+      process.chdir('test/graphqlconfig/multiproject-literal');
+    })
+
+    afterEach(() => {
+      process.chdir('../../..');
+    })
+
+    ruleTester.run('validation works using multiple schemas from .graphqlconfig', rule, {
+      valid: [
+        {
+          options: [{ env: 'literal' }],
+          code: 'const x = ESLintPluginGraphQLFile`{ number, sum(a: 1, b: 1) }`;',
+          filename: 'first.graphql',
+        },
+        {
+          options: [{ env: 'literal' }],
+          code: 'const x = ESLintPluginGraphQLFile`{ hero(episode: NEWHOPE) { id, name } }`;',
+          filename: 'second.graphql',
+        },
+        {
+          options: [{ env: 'literal' }],
+          code: 'const x = ESLintPluginGraphQLFile`{ number, sum(a: 1, b: 1) }`;',
+          filename: 'excluded1.graphql',
+        },
+        {
+          options: [{ env: 'literal' }],
+          code: 'const x = ESLintPluginGraphQLFile`{ hero(episode: NEWHOPE) { id, name } }`;',
+          filename: 'excluded2.graphql',
+        },
+      ],
+
+      invalid: [
+        {
+          options: [{ env: 'literal' }],
+          code: 'const x = ESLintPluginGraphQLFile`{ number, sum(a: 1, b: 1) }`;',
+          filename: 'second.graphql',
+          errors: [{
+            message: /cannot query field .number./i,
+          }],
+        },
+        {
+          options: [{ env: 'literal' }],
+          code: 'const x = ESLintPluginGraphQLFile`{ hero(episode: NEWHOPE) { id, name } }`;',
+          filename: 'first.graphql',
+          errors: [{
+            message: /cannot query field .hero./i,
+          }],
+        },
+      ],
+    });
+  });
 });
